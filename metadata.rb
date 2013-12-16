944f9f7a2862980c2644004a811d6f4da9da6e68
cosmo <cosmo_originstring@yahoo.co.jp>
2013-12-16 10:28:45 +0900

    fix typo

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Installs/Configures haskell-base'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-%w{ debisn ubuntu }.each do |os|
+%w{ debian ubuntu }.each do |os|
   supports os
 end
